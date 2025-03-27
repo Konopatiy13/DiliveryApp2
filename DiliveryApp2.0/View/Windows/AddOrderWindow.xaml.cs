bf9bb9385276702de25270a8240f72f83c7f5228
Konopatiy13 <160242043+Konopatiy13@users.noreply.github.com>
2025-03-27 22:33:28 +0300

    Добавьте файлы проекта.

---
+++ DiliveryApp2.0/View/Windows/AddOrderWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DiliveryApp2._0.ViewModel;
+
+namespace DiliveryApp2._0.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrderWindow.xaml
+    /// </summary>
+    public partial class AddOrderWindow : Window
+    {
+        public AddOrderWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = (DataContext as MainWindowViewModel).AddNewOrder();
+
+            if (result)
+            {
+                MessageBox.Show("Объект записан");
+                ((this.Owner as MainWindow).DataContext as MainWindowViewModel).LoadOrder();
+                this.Close();
+            }
+        }
+    }
+}
---
+++ DiliveryApp2.0/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DiliveryApp2._0.Model;
+
+namespace DiliveryApp2._0.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private string _name;
+        private int _customer_id;
+        private int restaurant_id;
+        private ObservableCollection<orders> _orders;
+        private orders _selectedOrder;
+        private orders _newOrder;
+
+        public string Name
+        {
+            get => _name;
+            set => SetPropertyChanged(ref _name, value, nameof(Name));
+        }
+
+        public int Course
+        {
+            get => _customer_id;
+            set => SetPropertyChanged(ref _customer_id, value, nameof(Course));
+        }
+
+        public ObservableCollection<orders> Orders
+        {
+            get => _orders;
+            set => SetPropertyChanged(ref _orders, value, nameof(Orders));
+        }
+
+        public orders SelectedOrder
+        {
+            get => _selectedOrder;
+            set => SetPropertyChanged(ref _selectedOrder, value, nameof(SelectedOrder));
+        }
+
+        public orders NewOrder
+        {
+            get => _newOrder;
+            set => SetPropertyChanged(ref _newOrder, value, nameof(NewOrder));
+        }
+
+
+        public MainWindowViewModel()
+        {
+            Orders = new ObservableCollection<orders>();
+            NewOrder = new orders();
+        }
+
+        public void LoadOrder()
+        {
+            Orders.Clear();
+            using (var context = new Dilivery2DBEntities())
+            {
+                var temp = context.orders.ToList();
+
+                foreach (var student in temp)
+                {
+                    Orders.Add(student);
+                }
+            }
+        }
+
+        public void DeleteOrder()
+        {
+            try
+            {
+                using (var context = new Dilivery2DBEntities())
+                {
+                    var findEntity = context.orders.FirstOrDefault(s => s.order_id == SelectedOrder.order_id);
+                    if (findEntity == null)
+                        return;
+                    var result = context.orders.Remove(findEntity);
+                    context.SaveChanges();
+
+                    LoadOrder();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public bool AddNewOrder()
+        {
+
+            using (var context = new Dilivery2DBEntities())
+            {
+                var newOrder = context.orders.Add(NewOrder);
+                context.SaveChanges();
+                return true;
+            }
+
+        }
+    }
+}
